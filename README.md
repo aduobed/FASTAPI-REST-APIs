b7bd6b5a9c453de9313f781dda353b022c7d1e8d
aduobed <obedus10@gmail.com>
2024-12-31 11:03:43 -0800

    add get method functionality

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 This is a simple RESTFUL FASTAPI application
+
+Start application using Uvicorn
+e.g uvicorn books:app --reload (This is for development purposes using `--reload`)
--- books.py
+++ books.py
@@ -1,8 +1,30 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
+BOOKS = {
+    "book_1": {"title": "title one", "author": "author one"},
+    "book_2": {"title": "title two", "author": "author two"},
+    "book_3": {"title": "title three", "author": "author three"},
+    "book_4": {"title": "title four", "author": "author four"},
+}
 
-@app.get("/")
+
+@app.get("/books")
 async def books():
-    return {'message': 'books'}
+    return BOOKS
+
+
+@app.get("/books/skip_book")
+async def skip_book(skip_book: Optional[str] = None):
+    if skip_book:
+        new_books = BOOKS.copy()
+        del new_books[skip_book]
+        return new_books
+    return BOOKS
+
+
+@app.get("/books/{book_id}")
+async def get_book_by_id(book_id: str):
+    return BOOKS.get(book_id)
