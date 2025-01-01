d3c4e68fc5d560b63d3be01c77054cf602cb5a88
aduobed <obedus10@gmail.com>
2025-01-01 13:48:01 -0800

    add json extra configs and fix id query parameter data type

--- books.py
+++ books.py
@@ -17,26 +17,43 @@ class Book(BaseModel):
         title="Description of the book", max_length=100, min_length=1, default=None)
     rating: int = Field(gt=-1, lt=11)
 
+    # Add a custom example to the json schema
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "id": "2a1de2ce-7899-44c4-9947-da248418a967",
+                    "title": "monkey king",
+                    "author": "james david",
+                    "description": "example of a description",
+                    "rating": 3}
+            ]
+        }
+    }
+
 
 @app.get("/books/")
-async def get_all_books():
+async def get_all_books(number_of_books: Optional[int] = None):
     if len(books) < 1:
         generate_book_data()
+
+    if number_of_books and len(books) >= number_of_books > 0:
+        return books[:number_of_books:]
     return books
 
 
 @app.get("/books/skip_book/")
 async def skip_book(skip_book: Optional[str] = None):
     if skip_book:
-        new_books = BOOKS.copy()
+        new_books = books.copy()
         del new_books[skip_book]
         return new_books
     return BOOKS
 
 
 @app.get("/books/{book_id}")
-async def get_book_by_id(book_id: str):
-    return BOOKS.get(book_id)
+async def get_book_by_id(book_id: UUID):
+    return [book for book in books if book.id == book_id]
 
 
 @app.post("/books/")
@@ -48,17 +65,18 @@ async def create_book(book: Book):
 
 
 @app.put("/books/{book_id}")
-async def update_book(book_id: str, title: str, author: str):
-    if BOOKS[book_id]:
-        BOOKS[book_id] = {"title": title, "author": author}
-
-    return BOOKS[book_id]
+async def update_book(book_id: UUID, book: Book):
+    get_book_index = int([idx for idx, val in enumerate(
+        books) if book_id == val.id][0])
+    books[get_book_index] = book
+    return books[get_book_index]
 
 
 @app.delete("/books/{book_id}")
-async def update_book(book_id: str):
-    if BOOKS[book_id]:
-        del BOOKS[book_id]
+async def update_book(book_id: UUID):
+    get_book_index = int([idx for idx, val in enumerate(
+        books) if book_id == val.id][0])
+    del books[get_book_index]
 
     return f"{book_id} has been deleted successfully"
 
